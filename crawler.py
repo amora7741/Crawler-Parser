e38ef1d66dcfd033cbe5d9aebca1ce686e5854ed
amora7741 <mora7741@gmail.com>
2023-11-19 12:53:38 -0800

    Implement frontier class

--- crawler.py
+++ crawler.py
@@ -1,7 +1,28 @@
 from urllib.request import urlopen
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['crawlerDB']
-pages_collection = db['pages']
+pages = db['pages']
+
+class Frontier:
+    def __init__(self, initial_url):
+        self.queue = [initial_url]
+        self.visited = set()
+
+    def addURL(self, url):
+        if url not in self.visited and url not in self.queue:
+            self.queue.append(url)
+
+    def nextURL(self):
+        if self.queue:
+            url = self.queue.pop(0)
+            self.visited.add(url)
+            return url
+        
+        return None
+
+    def done(self):
+        return not self.queue
