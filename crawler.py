fcde12bcb996b5881df1e67270690d3be5c7db7e
amora7741 <mora7741@gmail.com>
2023-11-19 12:18:18 -0800

    Setup mongodb connection

--- crawler.py
+++ crawler.py
@@ -1,3 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['crawlerDB']
+pages_collection = db['pages']
