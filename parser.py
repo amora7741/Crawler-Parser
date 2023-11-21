5a7fee190a769efcffaf5b2fbbf063de18e20e9b
amora7741 <mora7741@gmail.com>
2023-11-21 15:07:12 -0800

    Get target URL and html content

--- parser.py
+++ parser.py
@@ -5,3 +5,7 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client['crawlerDB']
 pages = db['pages']
+
+targetURL = pages.find_one({'url': 'https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml'})['url']
+html = urlopen(targetURL)
+soup = BeautifulSoup(html.read(), 'html.parser')
