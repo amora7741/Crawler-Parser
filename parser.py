aff19772ed94a9f6259d168ef0e14b314884410e
amora7741 <mora7741@gmail.com>
2023-11-21 16:27:38 -0800

    Remove urlopen, use html from DB

--- parser.py
+++ parser.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['crawlerDB']
 pages = db['pages']
 
-targetURL = pages.find_one({'url': 'https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml'})['url']
-html = urlopen(targetURL)
-soup = BeautifulSoup(html.read(), 'html.parser')
+targetURL = pages.find_one({'url': 'https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml'})
+html = targetURL['html']
+soup = BeautifulSoup(html, 'html.parser')
 
 professorInfo = []
 for div in soup.find_all("div", class_="clearfix"):
@@ -46,3 +45,4 @@ for div in soup.find_all("div", class_="clearfix"):
 db['professors'].insert_many(professorInfo)
 
 print("Faculty data inserted into MongoDB successfully.")
+print("fart")
